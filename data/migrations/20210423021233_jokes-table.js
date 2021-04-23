85bc3bfad9e692aae5b0e97bea61d3a7c8a017c7
Stone98 <stone18003@gmail.com>
2021-04-22 19:22:13 -0700

    data setup

---
+++ data/migrations/20210423021233_jokes-table.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("jokes", (table) => {
+    table.increments("joke_id");
+    table.text("joke").notNullable();
+    table.text("punchline").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("jokes");
+};
